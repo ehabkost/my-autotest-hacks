bc5790cc5c650476f9a6b32aea41f8263dcad509
Eduardo Habkost <ehabkost@raisama.net>
2010-12-13 20:06:58 -0200

    kvm: use envobj on get_all_vms() call
    Signed-off-by: Eduardo Habkost <ehabkost@raisama.net>

--- client/tests/kvm/kvm/test.py
+++ client/tests/kvm/kvm/test.py
@@ -108,7 +108,7 @@ class KvmTest(test.test):
                 raise
             # Abort on error
             logging.info("Aborting job (%s)", e)
-            for vm in kvm_utils.env_get_all_vms(env):
+            for vm in kvm_utils.env_get_all_vms(envobj):
                 if vm.is_dead():
                     continue
                 logging.info("VM '%s' is alive.", vm.name)
